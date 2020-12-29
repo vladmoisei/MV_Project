9717eadbd5531c9e996507b99dc7ebba72cb23ff
unknown <vladmoisei@yahoo.com>
2020-12-29 22:05:43 +0200

    First Commit

---
+++ MV_Project/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MV_Project.Server.Data;
+using MV_Project.Server.Models;
+
+[assembly: HostingStartup(typeof(MV_Project.Server.Areas.Identity.IdentityHostingStartup))]
+namespace MV_Project.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
